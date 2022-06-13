60635ef5cccde082b826107420b485d70bac9c61
Yeonju-Kim <krust@krustui-MacBookPro-7.local>
2022-06-13 18:58:08 +0900

    Add event handling for wallet locked.

--- web3/index.js
+++ web3/index.js
@@ -1,4 +1,3 @@
-
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const fileInput = document.getElementById("fileUpload");
 const sendNFTButton = document.querySelector('.sendNFTButton');
@@ -15,6 +14,19 @@ const numOfNFTs = 3;
 const caver = new Caver("https://public-node-api.klaytnapi.com/v1/cypress");
 let myAccount;
 // Chain ID of Cypress: 8217 
+
+ethereum.on('accountsChanged',(accounts)=>{
+    document.getElementById('NFTs').style.display = "none"
+    if(!accounts.length){
+        document.getElementById('myAccountAddress').innerHTML =`Wallet locked!`;
+        ethereumButton.disabled = false;
+    }
+    else{
+        myAccount = accounts[0];
+        document.getElementById('myAccountAddress').innerHTML =`Wallet Address: ${myAccount}`;
+        ethereumButton.disabled = true;
+    }
+})
 
 ethereumButton.addEventListener('click', async () => {
     ethereumButton.disabled = true;   
@@ -90,34 +102,40 @@ async function updateNFTImage(index){
     }
 
     const nftContract = new web3.eth.Contract(JsonInterface, contractAddress)
-    totalBalance = await nftContract.methods.balanceOf(myAccount).call({from: myAccount })
+    try{
+        totalBalance = await nftContract.methods.balanceOf(myAccount).call({from: myAccount })
 
-    tokenIDlist = []
-    let tokenURIList = []
-    for (let i = 0; i < numOfNFTs ; i++)
-    {
-        if((i+index) < totalBalance)
+        tokenIDlist = []
+        let tokenURIList = []
+        for (let i = 0; i < numOfNFTs ; i++)
         {
-            tokenIDlist.push(await nftContract.methods.tokenOfOwnerByIndex(myAccount, i+index).call({from: myAccount}))
-            tokenURIList.push(await nftContract.methods.tokenURI(tokenIDlist[i]).call({from: myAccount}))
+            if((i+index) < totalBalance)
+            {
+                tokenIDlist.push(await nftContract.methods.tokenOfOwnerByIndex(myAccount, i+index).call({from: myAccount}))
+                tokenURIList.push(await nftContract.methods.tokenURI(tokenIDlist[i]).call({from: myAccount}))
+            }
         }
-    }
 
-    for (let i = 0 ; i < numOfNFTs ; i ++ )
-    {
-        if ((i + index) < totalBalance) 
+        for (let i = 0 ; i < numOfNFTs ; i ++ )
         {
-            document.getElementById(`${i+1}`).style.display="inline"
-            await fetch(tokenURIList[i])
-            .then(response => response.json())
-            .then((data) => {
-                document.getElementById(`${i+1}`).src= data.image;
-            });
+            if ((i + index) < totalBalance) 
+            {
+                document.getElementById(`${i+1}`).style.display="inline"
+                await fetch(tokenURIList[i])
+                .then(response => response.json())
+                .then((data) => {
+                    document.getElementById(`${i+1}`).src= data.image;
+                });
+            }
+            else{
+                document.getElementById(`${i+1}`).style.display="none"
+            }
         }
-        else{
-            document.getElementById(`${i+1}`).style.display="none"
-        }
+    }catch(e){
+        console.log(e)
+        return false;
     }
+    return true;
 }
 
 showNFTButton.addEventListener('click', async()=>{
@@ -127,10 +145,13 @@ showNFTButton.addEventListener('click', async()=>{
         console.log('Address has not set yet.')
         return;
     }
-    document.getElementById('NFTs').style.display = "inline"
-    setEvents();
-    page = 0;
-    await updateNFTImage(page);
+    let success = await updateNFTImage(page);
+    if (success)
+    {
+        document.getElementById('NFTs').style.display = "inline"
+        setEvents();
+        page = 0;
+    }
 })
 
 sendNFTButton.addEventListener('click', async() =>{
